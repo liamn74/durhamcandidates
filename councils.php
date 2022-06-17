89001f52384d7a90b576a1d1296c70b66fdfafa6
Liam Nichols <55455907+liamn74@users.noreply.github.com>
2022-06-17 03:16:51 +0000

    Added new candidate.

---
+++ councils.php
@@ -0,0 +1,105 @@
+<?php
+echo'<head>
+<link rel="icon" type="image/x-icon" href="/favicon.jpg">
+<meta property="og:url"                content="liamnichols.ca" />
+<meta property="og:title"              content="Durham Region Candidates Guide" />
+<meta property="og:description"        content="A comprehensive list of candidates running for municipal office across Durham Region" />
+<meta property="og:image"              content="https://www.liamnichols.ca/social.jpg" />
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HF8EHH9CXQ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag("js", new Date());
+
+  gtag("config", "G-HF8EHH9CXQ");
+</script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>';
+
+echo '
+<script>
+$(document).ready(function(){
+  $("[data-toggle='."tooltip".']").tooltip();   
+});
+</script>
+<title>Durham Region Candidates Guide</title>
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:creator" content="@whitbynerd">
+<meta name="twitter:title" content="Durham Region Candidates Guide">
+<meta name="twitter:description" content="A comprehensive list of candidates running for municipal office across Durham Region.">
+<meta name="twitter:image" content="https://www.liamnichols.ca/social.jpg">
+</head>
+<body>
+<div class="jumbotron" style="background-color:midnightblue;color:white;">
+<div class="container-fluid">
+<div class = "row">
+<div class = "col-sm-1"></div>
+<div class = "col-sm-8">
+<h1>Durham Region Candidates Guide</h1>
+A comprehensive list of candidates running for municipal office across Durham Region.
+</div>
+<div class = "col-sm-1"></div>
+</div>
+</div>
+</div>
+<div class="container-fluid" >
+<div class="row">
+<div class = "col-sm-1"></div>
+<div class = "col-sm-10">
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Candidates for Regional Chair | Municipality of Durham</li>
+  </ol>
+</nav>
+</div>
+<div class = "col-sm-1"></div>
+</div>
+<div class="row">
+<div class = "col-sm-1"></div>
+<div class="col-md-5">
+
+<h1>Council Positions</h1>
+<h1><small class="text-muted">Municipality of Durham</small>
+</h1>
+<hr/>
+<div class="panel panel-default">
+      <div class="panel-heading">Position Information:
+      </div>
+      <div class="panel-body" style ="overflow-x:auto;">
+<dl class="row">
+  <dt class="col-sm-3">Head of Council</dt>
+  <dd class="col-sm-9">Regional Chair <span class="badge">1 Position</span><hr></dd>
+
+  <dt class="col-sm-3">Council Members</dt>
+  <dd class="col-sm-9"><dl class="row"><dt class = "col-xs-3">1 Position<br>1 Position<br>1 Position<br>1 Position<br>4 Positions</dt><dd class = "col-xs-9">East Ward Councillor<br>West Ward Councillor<br>Centre Ward Councillor<br>North Ward Councillor<br>Regional Councillor</dl></dd></dd><hr>
+
+  <dt class="col-sm-3">Eligible Electors in the Previous Election</dt>
+  <dd class="col-sm-9">800,000<hr></dd>
+
+  <dt class="col-sm-3 text-truncate">Truncated term is truncated</dt>
+  <dd class="col-sm-9">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.<hr></dd>
+
+  <dt class="col-sm-3">Nesting</dt>
+  <dd class="col-sm-9">Yes!</dd>
+</dl>
+      </div>
+</div>
+</div>
+<div class="col-md-5">
+<h1>Ward Map</h1>
+
+</h1>
+</div>
+</div>
+</div>
+</div>
+
+
+';
+
+?>
